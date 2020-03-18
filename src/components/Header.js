7cb58a999e361a447e1db1314df8e2b51a8a4eff
AmenoCoyane <bobbidigi11@gmail.com>
2020-03-18 14:37:21 -0400

    day 2 completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,6 @@ import React from 'react';
 export default function Header(props) {
     return (
         <div>
-            <p>
-                sup gaymers this is the header cool cool cool(real header)
-            </p>
-            <p>
-                sup(real sup) {props.sup}
-            </p>
         </div>
     )
 }
--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -5,15 +5,26 @@ import styled from 'styled-components';
 const GreyH1 = styled.h1`
   color: #666;
 `;
+
+const Image = styled.img`
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+`;
+
+const Explanation = styled.p`
+    color: #666;
+    padding: 20px;
+`;
+
 export default function Paragraph(props) {
     console.log(props)
     return (
         <div>
             <GreyH1>{props.data.title}</GreyH1>
-            <p>
-                
-            </p>
-            <img src={props.data.hdurl}/>
+            <Image src={props.data.hdurl}></Image>
+            <Explanation>{props.data.explanation}</Explanation>
         </div>
     )
 }
